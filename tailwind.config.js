caf1878bc4b4031c6cf38d9b3e956ab0ea4fc0e3
powtzulist <delasanarwin@gmail.com>
2025-01-21 14:51:51 +0800

    [colors] added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          white: "#ffffff",
+          green: "#e1f1e8",
+        },
+        secondary: {
+          accentGreen: "#c2e8d3",
+          yellow: "#f4ef62",
+          lightyellow: "#fffee1",
+        },
+        tertiary: {
+          buttonGreen: "#37b16e",
+          buttonBlue: "#c6dfff",
+          buttonRed: "#bf5e5e",
+          textBlue: "#146fe1",
+          textRed: "#e31e1e",
+          textGray: "#6B6B6B",
+          textYellow: "#a09d45",
+        },
+      },
+    },
   },
   plugins: [],
 };
