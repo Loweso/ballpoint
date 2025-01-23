832740a55421c5b7e17137b6a842283637339194
Loweso <dlouisefermin@gmail.com>
2025-01-24 00:04:00 +0800

    [Louise] working filter and sort frontend, not yet made into components

--- app/index.tsx
+++ app/index.tsx
@@ -20,6 +20,9 @@ import DropDownPicker from "react-native-dropdown-picker";
 import { CreateNewNoteModal } from "@/components/CreateNewNoteModal";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { DatePickerModal } from "react-native-paper-dates";
+import { format } from "date-fns";
+
 DropDownPicker.setMode("BADGE");
 
 export default function Index() {
@@ -30,6 +33,10 @@ export default function Index() {
     { label: "Category 2", value: "category2" },
     { label: "Category 3", value: "category3" },
   ]);
+
+  const clearCategories = () => {
+    setValue(null);
+  };
 
   const [isFilterMenuVisible, setFilterMenuVisible] = useState(false);
   const [isSortMenuVisible, setSortMenuVisible] = useState(false);
@@ -62,6 +69,28 @@ export default function Index() {
     }).start();
   };
 
+  const [pressedSortType, setPressedSortType] = useState<string | null>(null);
+  const [pressedSortOrder, setPressedSortOrder] = useState<string | null>(null);
+
+  const handleSortTypePress = (button: string) => {
+    if (button === pressedSortType) {
+      setPressedSortType(null);
+    } else {
+      setPressedSortType(button);
+    }
+  };
+
+  const handleSortOrderPress = (button: string) => {
+    if (button === pressedSortOrder) {
+      setPressedSortOrder(null);
+    } else {
+      setPressedSortOrder(button);
+    }
+  };
+
+  const [isCreateNewNoteModalVisible, setIsCreateNewNoteModalVisible] =
+    useState(false);
+
   const [range, setRange] = React.useState<{
     startDate: Date | undefined;
     endDate: Date | undefined;
@@ -70,11 +99,15 @@ export default function Index() {
     endDate: undefined,
   });
 
-  const [isModalOpen, setisModalOpen] = React.useState(false);
+  const [isDateModalOpen, setisDateModalOpen] = React.useState(false);
 
   const onDismiss = React.useCallback(() => {
-    setOpen(false);
-  }, [setOpen]);
+    setisDateModalOpen(false);
+  }, [setisDateModalOpen]);
+
+  const clearDateRange = () => {
+    setRange({ startDate: undefined, endDate: undefined });
+  };
 
   const onConfirm = React.useCallback(
     ({
@@ -84,14 +117,11 @@ export default function Index() {
       startDate: Date | undefined;
       endDate: Date | undefined;
     }) => {
-      setOpen(false);
+      setisDateModalOpen(false);
       setRange({ startDate, endDate });
     },
-    [setOpen, setRange]
+    [setisDateModalOpen, setRange]
   );
-
-  const [isCreateNewNoteModalVisible, setIsCreateNewNoteModalVisible] =
-    useState(false);
 
   const content = <Ionicons name="add-outline" size={50} color="black" />;
   return (
@@ -150,29 +180,94 @@ export default function Index() {
       >
         <Text className="text-black font-bold my-3">FILTER BY</Text>
         <Text className="text-black">Category</Text>
-        <DropDownPicker
-          open={open}
-          value={value}
-          items={items}
-          setOpen={setOpen}
-          setValue={setValue}
-          setItems={setItems}
-          multiple={true}
-          min={0}
-          max={5}
-          showBadgeDot={false}
-          placeholder="None chosen"
-        />
+
+        <View className="flex flex-row items-center justify-between w-full h-12 bg-transparent">
+          <View className="flex flex-row items-center justify-between w-[87.5%] bg-transparent">
+            <DropDownPicker
+              open={open}
+              value={value}
+              items={items}
+              setOpen={setOpen}
+              setValue={setValue}
+              setItems={setItems}
+              multiple={true}
+              min={0}
+              max={5}
+              showBadgeDot={false}
+              placeholder="None chosen"
+            />
+          </View>
+
+          <TouchableOpacity
+            className="items-start justify-center bg-transparent w-8"
+            onPress={clearCategories}
+          >
+            <Entypo name="erase" size={20} color="gray" />
+          </TouchableOpacity>
+        </View>
 
         <Text className="text-black mt-3">Date</Text>
+        <View className="flex flex-row items-center justify-between w-full h-12 bg-transparent">
+          <Text>
+            {range.startDate && range.endDate
+              ? `${format(
+                  new Date(range.startDate),
+                  "MMM dd, yyyy"
+                )} - ${format(new Date(range.endDate), "MMM dd, yyyy")}`
+              : "Select a date range"}
+          </Text>
 
-        <View className="flex flex-col items-center w-full">
+          <View className="flex flex-row items-center gap-3">
+            <Pressable
+              className="w-36 h-12 flex items-center justify-center bg-secondary-yellow rounded-full"
+              onPress={() => setisDateModalOpen(true)}
+            >
+              <Text className="font-bold">Pick date range</Text>
+            </Pressable>
+
+            <TouchableOpacity
+              className="items-start justify-center bg-transparent w-8"
+              onPress={() => clearDateRange()}
+            >
+              <Entypo name="erase" size={20} color="gray" />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <DatePickerModal
+          locale="en"
+          mode="range"
+          visible={isDateModalOpen}
+          onDismiss={onDismiss}
+          startDate={range.startDate}
+          endDate={range.endDate}
+          onConfirm={onConfirm}
+        />
+
+        <View className="flex flex-col items-center w-full mt-4 gap-3">
           <Pressable
-            className="flex flex-col items-center justify-center w-1/2 h-16 bg-tertiary-buttonGreen rounded-full hover:bg-[#37b16f]"
-            onPress={() => console.log("Apply Filters Pressed")}
+            className={`flex flex-col items-center justify-center w-1/2 h-16 rounded-full ${
+              value != null ||
+              (range.startDate !== undefined && range.endDate !== undefined)
+                ? "bg-tertiary-buttonGreen hover:bg-[#37b16f]"
+                : "bg-gray-400"
+            }`}
+            onPress={() => console.log(value, "date range here")}
+            disabled={
+              value == null &&
+              range.startDate === undefined &&
+              range.endDate === undefined
+            }
           >
             <Text className="text-primary-white font-bold">APPLY FILTERS</Text>
           </Pressable>
+
+          <TouchableOpacity
+            className="items-start justify-center bg-transparent w-8"
+            onPress={toggleFilterMenu}
+          >
+            <Entypo name="chevron-thin-up" size={20} color="gray" />
+          </TouchableOpacity>
         </View>
       </Animated.View>
 
@@ -184,22 +279,50 @@ export default function Index() {
       >
         <Text className="text-black font-bold my-3">SORT BY</Text>
         <View className="flex flex-row justify-between w-full">
-          <Pressable className="flex flex-col items-center justify-center bg-primary-white h-24 w-24 shadow-lg shadow-black gap-2">
+          <Pressable
+            onPress={() => handleSortTypePress("Category")}
+            className={`flex flex-col items-center justify-center bg-primary-white h-24 w-1/3 shadow-lg gap-2 ${
+              pressedSortType === "Category"
+                ? "border-2 bg-gray-100"
+                : "shadow-black bg-primary-white"
+            }`}
+          >
             <MaterialIcons name="category" size={25} color="black" />
             <Text>Category</Text>
           </Pressable>
-          <Pressable className="flex flex-col items-center justify-center bg-primary-white h-24 w-24 shadow-lg shadow-black gap-2">
+          <Pressable
+            onPress={() => handleSortTypePress("Date")}
+            className={`flex flex-col items-center justify-center h-24 w-1/3 shadow-lg gap-2 ${
+              pressedSortType === "Date"
+                ? "border-2 bg-gray-100"
+                : "shadow-black bg-primary-white"
+            }`}
+          >
             <Entypo name="calendar" size={25} color="black" />
             <Text>Date</Text>
           </Pressable>
-          <Pressable className="flex flex-col items-center justify-center bg-primary-white h-24 w-24 shadow-lg shadow-black gap-2">
+          <Pressable
+            onPress={() => handleSortTypePress("Title")}
+            className={`flex flex-col items-center justify-center bg-primary-white h-24 w-1/3 shadow-lg gap-2 ${
+              pressedSortType === "Title"
+                ? "border-2 bg-gray-100"
+                : "shadow-black bg-primary-white"
+            }`}
+          >
             <MaterialIcons name="title" size={25} color="black" />
             <Text>Title</Text>
           </Pressable>
         </View>
 
         <View className="flex flex-row justify-between w-full">
-          <Pressable className="flex flex-row items-center justify-center bg-primary-white h-24 w-1/2 gap-4">
+          <Pressable
+            onPress={() => handleSortOrderPress("SortAscending")}
+            className={`flex flex-row items-center justify-center h-24 w-1/2 gap-4 ${
+              pressedSortOrder === "SortAscending"
+                ? "border-2 bg-gray-100"
+                : "shadow-black bg-primary-white"
+            }`}
+          >
             <MaterialCommunityIcons
               name="sort-alphabetical-ascending"
               size={25}
@@ -207,7 +330,14 @@ export default function Index() {
             />
             <Text className="font-bold">Sort Ascending</Text>
           </Pressable>
-          <Pressable className="flex flex-row items-center justify-center bg-primary-white h-24 w-1/2 gap-4">
+          <Pressable
+            onPress={() => handleSortOrderPress("SortDescending")}
+            className={`flex flex-row items-center justify-center h-24 w-1/2 gap-4 ${
+              pressedSortOrder === "SortDescending"
+                ? "border-2 bg-gray-100"
+                : "shadow-black bg-primary-white"
+            }`}
+          >
             <MaterialCommunityIcons
               name="sort-alphabetical-descending"
               size={25}
@@ -217,13 +347,25 @@ export default function Index() {
           </Pressable>
         </View>
 
-        <View className="flex flex-col items-center w-full">
+        <View className="flex flex-col items-center w-full gap-3">
           <Pressable
-            className="flex flex-col items-center justify-center w-1/2 h-16 bg-tertiary-buttonGreen rounded-full hover:bg-[#37b16f]"
-            onPress={() => console.log("Apply Filters Pressed")}
+            className={`flex flex-col items-center justify-center w-1/2 h-16 rounded-full ${
+              pressedSortType && pressedSortOrder
+                ? "bg-tertiary-buttonGreen hover:bg-[#37b16f]"
+                : "bg-gray-400"
+            }`}
+            onPress={() => console.log(pressedSortType, pressedSortOrder)}
+            disabled={!pressedSortType || !pressedSortOrder}
           >
             <Text className="text-primary-white font-bold">APPLY SORT</Text>
           </Pressable>
+
+          <TouchableOpacity
+            className="items-start justify-center bg-transparent w-8"
+            onPress={toggleSortMenu}
+          >
+            <Entypo name="chevron-thin-up" size={20} color="gray" />
+          </TouchableOpacity>
         </View>
       </Animated.View>
 
