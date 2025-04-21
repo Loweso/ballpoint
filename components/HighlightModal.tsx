810307a69649e05eb544d92922b028fa6f91ab14
Renier Jardio <renierjardio@gmail.com>
2025-04-21 15:56:57 +0800

    clicking outide highlight modal closes it

--- components/HighlightModal.tsx
+++ components/HighlightModal.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { View, TouchableOpacity, Keyboard, Dimensions } from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Dimensions,
+} from "react-native";
 import { FontAwesome6, Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 import QueryMenuModal from "./QueryMenuModal";
@@ -20,10 +26,10 @@ export const HighlightModal: React.FC<HighlightModalProps> = ({
 
   const [isQueryMenuModal, setIsQueryMenuModalVisible] = useState(false);
   const screen = Dimensions.get("window");
-  const MODAL_WIDTH = 120;
+  const MODAL_WIDTH = 85;
   const MODAL_HEIGHT = 36;
 
-  let top = position.top + 10;
+  let top = position.top + 15;
   let left = position.left;
 
   if (left < 10) left = 10;
@@ -39,64 +45,68 @@ export const HighlightModal: React.FC<HighlightModalProps> = ({
   };
 
   return (
-    <View
-      className="absolute z-100 bg-white rounded-2xl shadow-md items-center"
-      style={{
-        top,
-        left,
-        width: MODAL_WIDTH,
-        height: MODAL_HEIGHT,
-      }}
-    >
-      <View className="flex flex-row justify-content-center">
-        <TouchableOpacity
-          className="mx-1 mt-2"
-          onPress={() => {
-            console.log("Highlight text");
+    <View className="absolute inset-0 z-50">
+      <TouchableWithoutFeedback onPress={closeModal}>
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            zIndex: 49,
           }}
-        >
-          <FontAwesome6 name="highlighter" size={22} color="gray" />
-        </TouchableOpacity>
+        />
+      </TouchableWithoutFeedback>
 
-        <View className="w-[1%] h-[65%] m-1 mt-2 bg-[#D3D3D3] rounded" />
+      <View
+        className="absolute z-100 bg-white rounded-2xl shadow-md items-center"
+        style={{
+          top,
+          left,
+          width: MODAL_WIDTH,
+          height: MODAL_HEIGHT,
+        }}
+      >
+        <View className="flex flex-row justify-content-center">
+          <TouchableOpacity
+            className="mx-1 mt-2"
+            onPress={() => {
+              console.log("Highlight text");
+            }}
+          >
+            <FontAwesome6 name="highlighter" size={22} color="gray" />
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          className="mx-1 mt-1"
-          onPress={() => {
-            setIsQueryMenuModalVisible(true);
-            console.log("Open AI enhance features");
-          }}
-        >
-          <LinearGradient
-            colors={["#146fe1", "#37b16e"]}
-            style={{ width: 30, height: 30, borderRadius: 20 }}
-          />
-          <FontAwesome6
-            className="absolute ml-[4px] mt-[4px]"
-            name="wand-magic-sparkles"
-            size={20}
-            color="white"
-          />
-        </TouchableOpacity>
+          <View className="w-[1%] h-[65%] m-1 mt-2 bg-[#D3D3D3] rounded" />
 
-        <View className="w-[1%] h-[65%] m-1 mt-2 bg-[#D3D3D3] rounded" />
+          <TouchableOpacity
+            className="mx-1 mt-1"
+            onPress={() => {
+              setIsQueryMenuModalVisible(true);
+              console.log("Open AI enhance features");
+            }}
+          >
+            <LinearGradient
+              colors={["#146fe1", "#37b16e"]}
+              style={{ width: 30, height: 30, borderRadius: 20 }}
+            />
+            <FontAwesome6
+              className="absolute ml-[4px] mt-[4px]"
+              name="wand-magic-sparkles"
+              size={20}
+              color="white"
+            />
+          </TouchableOpacity>
+        </View>
 
-        <TouchableOpacity
-          className="mx-1 mt-2"
-          onPress={() => {
-            closeModal();
-          }}
-        >
-          <Ionicons name="close-circle-outline" size={22} color="#bf5e5e" />
-        </TouchableOpacity>
+        <QueryMenuModal
+          visible={isQueryMenuModal}
+          onClose={closeModal}
+          onProcessQuery={closeModal}
+          onCompleteHighlightedText={closeModal}
+        />
       </View>
-
-      <QueryMenuModal
-        visible={isQueryMenuModal}
-        onClose={closeModal} // change this in future
-        onProcessQuery={closeModal} // change this in future
-        onCompleteHighlightedText={closeModal} // change this in future
-      />
     </View>
   );
 };
